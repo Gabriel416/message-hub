b37c3a81acabb8be85555eeee117379f1aabe3f0
Gabriel416 <gferguson954@gmail.com>
2018-10-10 21:37:12 -0400

    init sms functionality

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
+const smsRouter = require('./routes/sms');
+const emailRouter = require('./routes/email');
+
+const app = express();
+const apiPrefix = '/api/v1';
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+app.use(`${apiPrefix}/sms`, smsRouter);
+app.use(`${apiPrefix}/email`, emailRouter);
+
+module.exports = app;
---
+++ routes/sms.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const Nexmo = require("nexmo");
+const phoneUtil = require("phone");
+const router = express.Router();
+
+/* send sms */
+router.post("/", (req, res, next) => {
+  console.log(req.body, "REQ");
+  let { phone, message } = req.body;
+
+  const formattedPhone = phoneUtil(phone)[0];
+  if (!formattedPhone) {
+    //   validation fails
+    res.status(500).send("Invalid phone number");
+  }
+
+  const nexmo = new Nexmo({
+    apiKey: process.env.NEXMO_KEY,
+    apiSecret: process.env.NEXMO_SECRET,
+  });
+
+  const to = formattedPhone;
+  const text = message;
+
+  nexmo.message.sendSms(process.env.NEXMO_VIRTUAL_NUMBER, to, text, (error, response) => {
+    if (error) {
+      res.status(500).send(error);
+      throw error;
+    } else if (response.messages[0].status != "0") {
+      res.json("Server returned back a non-zero status");
+      throw "Nexmo returned back a non-zero status";
+    } else {
+      res.json(response);
+      console.log(response);
+    }
+  });
+});
+
+module.exports = router;
